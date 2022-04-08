85523c9ad167a63406d375430d512995c579e553
unknown <xiaoxm_001@outlook.com>
2022-04-09 07:54:35 +0800

    re sub

--- cainiao1/test_re2.py
+++ cainiao1/test_re2.py
@@ -1,26 +1,33 @@
 import re
 
+
 def test_split():
     data1 = 'you     are \
         the best'
-    res_list=re.split(r'\s+',data1)
-    assert ['you','are','the','best']==res_list,'re split error'
-    
+
+    res_list = re.split(r'\s+', data1)
+    assert ['you', 'are', 'the', 'best'] == res_list, 're split error'
+
 
 def test_sub():
-    data1 ='my mobile is 15811112222'
-    num = re.sub(r'^.*is ','',data1)
-    assert '15811112222'==num,'re sub error'
-    
-    num = re.sub(r'\D+','',data1)
-    assert '15811112222'==num,'re sub error'
+    data1 = 'my mobile is 15811112222'
+    num = re.sub(r'^.*is ', '', data1)
+    assert '15811112222' == num, 're sub 1 error'
+
+    num = re.sub(r'\D+', '', data1)
+    assert '15811112222' == num, 're sub 2 error'
+
+    # 如果分组, 在组外面的数字也是包含在内的
+    num = re.sub(r'(.*?)is ', '', data1)
+    assert '15811112222' == num, 're sub 3 error'
+
 
 def test_sub2():
     data1 = 'a:1,b:2,c:3'
+
     def multi(matched):
         v = int(matched.group())
-        return str(v*2)
-    
-    res=re.sub(r'\d+',multi,data1)
-    assert 'a:2,b:4,c:6'==res,'re sub2 error'
-    
+        return str(v * 2)
+
+    res = re.sub(r'\d+', multi, data1)
+    assert 'a:2,b:4,c:6' == res, 're sub 4 error'
